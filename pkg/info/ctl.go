8a585acb99e5105f9d49a897f6382fc551b58d92
Nikita Kulakov <klephron.dev@gmail.com>
2026-01-05 18:11:03 +0300

    update info

--- pkg/info/ctl.go
+++ pkg/info/ctl.go
@@ -2,6 +2,6 @@ package info
 
 const (
 	CtlName             string = "mals-ctl"
-	CtlVersion          string = "0.0.1"
-	CtlDescriptionShort string = "CTL application for mals"
+	CtlVersion          string = "0.1.0"
+	CtlDescriptionShort string = "Controls model augment language servers (MALS)."
 )
